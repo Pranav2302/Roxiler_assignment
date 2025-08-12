10f16bc08460b8fe52d08905a1620622de5ff4d5
Pranav2302 <21118023@pvgcoet.ac.in>
2025-08-12 19:08:09 +0530

    up

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = process.env.NODE_ENV==='production'?'http://localhost:5000/api' :"https://roxiler-assignment-backend-nine.vercel.app/api";
+const API_BASE = process.env.NODE_ENV==='production'?"https://roxiler-assignment-backend-nine.vercel.app/api" :'http://localhost:5000/api';
 
 export const setAuthToken = (token) => {
     if(token){
